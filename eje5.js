7b2e2bd8dec352a223dd611ea1f5add102d27beb
Octavio <octaviodiaz262@gmail.com>
2023-05-09 18:58:27 -0300

    Ejercicio 5

---
+++ eje5.js
@@ -0,0 +1,11 @@
+var enlace = document.getElementsByTagName("a");
+
+var parrafos = document.getElementsByTagName("p");
+
+for(var i=0; i<parrafos.length; i++){
+    var parrafo = parrafos[i]
+}
+var enlace1 = parrafo.getElementsByTagName("a");
+document.writeln ("La pagina tiene "+enlace.length+ " enlaces de pagina "+ " <br> ");
+
+document.writeln(" El tercer parrafo tiene "+enlace1.length+ " enlaces " );
