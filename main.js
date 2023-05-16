80f8985457d16f8cdbb8c6e5e9852a32d8e39862
Octavio <octaviodiaz262@gmail.com>
2023-05-16 18:58:44 -0300

    Ejercicio 7 1.1

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ pY = a.pageY;
 document.getElementById('raton').innerHTML= "Navegador" +"[ " +movX + "," + movY +"]";
 document.getElementById('raton2').innerHTML= "Pagina" +"[ " +pX + "," + pY +"]";
 })
+
+let x;
+let c;
+document.addEventListener('keypress', function(b){
+x = b.key 
+c = b.charCode
+document.getElementById('teclado').innerHTML= "Caracter" +"[ " +x + "]";
+document.getElementById('teclado2').innerHTML= "Codigo" +"[ " +c + "]";
+})
